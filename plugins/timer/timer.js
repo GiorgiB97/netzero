829d326a6bef4835ad3bc26d7f49257a64c648a5
Giorgi Batiashvili <giorgi.batiashvili@gmail.com>
2017-12-12 19:57:23 +0400

    layouts changed

---
+++ plugins/timer/timer.js
@@ -0,0 +1,27 @@
+var countDownDate = new Date("Jan 1, 2018 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+    var current = new Date().getTime();
+
+    var range = countDownDate - current;
+
+    var daysTo = Math.floor(range / (1000 * 60 * 60 * 24));
+    var hoursTo = Math.floor((range % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutesTo = Math.floor((range % (1000 * 60 * 60)) / (1000 * 60));
+    var secondsTo = Math.floor((range % (1000 * 60)) / 1000);
+
+    document.getElementById("daysTo").innerHTML = daysTo;
+    document.getElementById("hoursTo").innerHTML = hoursTo;
+    document.getElementById("minutesTo").innerHTML = minutesTo;
+    document.getElementById("secondsTo").innerHTML = secondsTo;
+
+    if (range < 0) {
+        clearInterval(x);
+        document.getElementById("daysTo").innerHTML = "00";
+        document.getElementById("hoursTo").innerHTML = "00";
+        document.getElementById("minutesTo").innerHTML = "00";
+        document.getElementById("secondsTo").innerHTML = "00";
+
+    }
+}, 1000);
